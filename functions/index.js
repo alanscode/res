5118581657af2e467003537839d11541a2480839
alanscode <alnngyn@gmail.com>
2018-03-03 23:28:48 -0800

    test

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,44 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 const express = require('express')
 var bodyParser = require('body-parser');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({
+    origin: true
+});
 
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 app.use(cors)
 
 admin.initializeApp(functions.config().firebase)
 
-app.post('/contactform',(req,res) =>{
-    if(req.body.alan != '@l@n') {
-        
+app.post('/contactform', (req, res) => {
+    if (req.body.alan != '@l@n') {
+
         res.end("Unauthorized Access")
         return
     }
-    
+
     delete req.body.alan
 
     let message = req.body
 
-   admin.database().ref("contactform").push(message)
-   res.end("OK")
+    admin.database().ref("contactform").push(message)
+    res.end("OK")
 })
+
+app.get('/list', (req, res) => {
+    console.log('function called')
+    admin.database().ref("contactform").orderByKey()
+        .once("value")
+        .then(messages => {
+            var result = JSON.stringify(messages.val())
+            res.end(result)
+        })
+
+})
+
 
 exports.message = functions.https.onRequest(app);
